d22f30395d9984af4dfd1f7b26eb6bc613710c7b
muradav <muradav@code.edu.az>
2022-10-12 23:51:41 +0400

    will be continued

---
+++ FinalProjectApi/RentACarProject/RentACarProject/Entities/CarImage.cs
@@ -0,0 +1,16 @@
+﻿namespace RentACarProject.Entities
+{
+    public class CarImage:BaseEntity
+    {
+        public string ImageUrl { get; set; }
+        public bool IsMain { get; set; }
+
+
+
+        #region CarImageCarRelation
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        #endregion
+
+    }
+}
